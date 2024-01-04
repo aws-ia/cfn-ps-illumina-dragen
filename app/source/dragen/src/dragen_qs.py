aws-ia/cfn-ps-illumina-dragen

commit cbe52ba45bf96711f6181c33fc5716cfb771714d

Address cfn-lint issues

app/source/dragen/src/dragen_qs.py
@@ -79,13 +79,11 @@ def find_arg_in_list(arglist, *argv):
 #########################################################################################
 # exec_cmd - Execute command and return [stdout/stderr, exitstatus]
 #
-def exec_cmd(cmd, shell=True):
+def exec_cmd(cmd):
     printf("Executing %s" % cmd.strip())
 
     if not shell:
         p = subprocess.Popen(cmd.split())
-    else:
-        p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
 
     err = p.wait()
     return err

commit 426f6ae7c8713ac64687be319266b1dc9e3f0aa9

Fix redirection in dragen_qs.py
As the script uses redirection operator in commands, keep shell around
to properly support that.

Fixes: cbe52ba45bf9 ("Address cfn-lint issues")
Fixes: f78c1129dbd3 ("Updated dragen_qs.py")

app/source/dragen/src/dragen_qs.py
@@ -81,7 +81,7 @@ def find_arg_in_list(arglist, *argv):
 #
 def exec_cmd(cmd):
     printf("Executing %s" % cmd.strip())
-    p = subprocess.Popen(cmd.split())
+    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
     err = p.wait()
     return err
 